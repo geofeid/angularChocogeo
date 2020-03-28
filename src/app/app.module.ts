e7d1ac04781eca717e6f4374e1a8b24d6ed1529b
geofeid <fidakisgeorge@yahoo.com>
2020-03-28 15:42:38 +0200

    Creating data- Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
@@ -31,7 +33,9 @@ import { FeedbackService } from './services/feedback.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [DishService,LeaderService,FeedbackService,
     {provide: 'BaseURL', useValue: baseURL}],
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Inject} from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { FeedbackService } from '../services/feedback.service';
 import { Feedback } from '../shared/feedback';
 
@@ -11,14 +12,34 @@ import { Feedback } from '../shared/feedback';
 export class ContactComponent implements OnInit {
 
   feedbacks: Feedback[];
+  feedbackForm: FormGroup;
+  fb: Feedback;
   
   constructor(private feedBackService: FeedbackService,
-    @Inject('BaseURL') public  BaseURL)
+    @Inject('BaseURL') public  BaseURL,
+    private fbformbuilding: FormBuilder) {this.createForm(); }
     
-     { }
-
-  ngOnInit() {
+    ngOnInit() {
     this.feedBackService.getFeedbacks().subscribe(feedbacks => this.feedbacks = feedbacks);
-  }
+    }
+    createForm() {
+      this.feedbackForm = this.fbformbuilding.group({
+      firstname: '',
+      lastname: '',
+      message: ''
+      });
+    }
+    
+    onSubmitFeedback() {
+      this.fb = this.feedbackForm.value;
+      console.log(this.fb);
+      this.feedbacks.push(this.fb);
+      this.feedbackForm.reset({
+        firstname: '',
+        lastname: '',
+        message: ''
+      });
+
+    }
 
 }
